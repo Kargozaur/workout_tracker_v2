a4d6cec4f3b9a5a36daa0a7dc62f40c6c8abde8b
Illia <lpodildaviduk@gmail.com>
2026-03-14 19:18:54 +0100

    class to receive the request data

---
+++ app/workout/domains/entities/httpx_response.py
@@ -0,0 +1,29 @@
+from pydantic.alias_generators import to_camel
+
+from . import BaseModel, ConfigDict, Field
+
+
+class ResponseBaseSchema(BaseModel):
+    model_config = ConfigDict(validate_by_alias=True, alias_generator=to_camel)
+
+
+class Metadata(ResponseBaseSchema):
+    total_exercises: int
+    total_pages: int
+    current_page: int
+    previous_page: str | None
+    next_page: str
+
+
+class Data(ResponseBaseSchema):
+    exercise_id: str
+    name: str
+    target_muscles: list[str]
+    body_parts: list[str]
+    instructions: list[str]
+
+
+class ResponseSchema(ResponseBaseSchema):
+    success: bool
+    metadata: Metadata
+    data: list[Data]
---
+++ app/workout/infrastructure/api_data/get_data.py
@@ -0,0 +1,63 @@
+# import asyncio
+import time
+from urllib.parse import urlparse
+
+from httpx import AsyncClient
+from loguru import logger
+
+from app.workout.domains.entities.httpx_response import ResponseSchema
+
+
+class APIData:
+    def __init__(
+        self,
+        client: AsyncClient,
+        url: str = "https://exercisedb.dev/api/v1/exercises",
+    ) -> None:
+        self.client = client
+        self.__api_url = url
+        self.__base_domain = self._get_domain(self.__api_url)
+
+    @staticmethod
+    def _get_domain(url: str) -> str:
+        return urlparse(url).netloc
+
+    @property
+    def api(self) -> str:
+        return self.__api_url
+
+    @api.setter
+    def api(self, new_url: str) -> None:
+        domain = self._get_domain(new_url)
+        if domain != self.__base_domain:
+            raise ValueError(
+                f"Domain {self.__base_domain} expected, instead got: {domain}"
+            )
+        self.__api_url = new_url
+
+    async def get_data(
+        self, offset: int = 0, limit: int = 10
+    ) -> ResponseSchema | None:
+        logger.info(f"requesting {self.api}")
+        start = time.perf_counter()
+        response = await self.client.get(
+            self.api, params={"offset": offset, "limit": limit}
+        )
+        if response.status_code == 200:
+            logger.info(
+                f"response status code: {response.status_code}.\n"
+                f"Response_time: {time.perf_counter() - start:.2f}ms"
+            )
+            data = ResponseSchema.model_validate_json(response.text)
+            return data
+        return None
+
+
+# async def get_data():
+#     async with AsyncClient() as aclient:
+#         new_data = APIData(aclient)
+#         res = await new_data.get_data()
+#     return res
+#
+#
+# print(asyncio.run(get_data()))
